db221fc130ad14a3e4a2f438076f5138cc0e1dad
Pradnya136 <pradnyasonawane133@gmail.com>
2024-07-21 23:44:37 +0530

    ystrdays solution

--- index.js
+++ index.js
@@ -55,3 +55,17 @@ let num_pali = num => {
 num_pali();
 
 // 4 - Write a JavaScript function that generates all combinations of a string.
+
+let combo = str => {
+    let result = [];
+
+    for(let i = 0;i < str.length; i++ ){
+        for(let j = i+1; j <= str.length; j++){
+            result.push(str.substring(i,j));
+        }
+
+    }
+    return result;
+
+    
+}
